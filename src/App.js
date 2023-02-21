67a903ed894c8f45d91a10a669570b79d377d137
bjucs <bju@umd.edu>
2023-02-21 07:55:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react"; 
+import "./App.css"; 
+
+/* 
+MacroTrack 
+developed by Brian Ju, UMD 2025 
+Frameworks/Languages: React, JSX 
+
+Instructions to run: 
+-cd into macrotrack
+-"npm start"
+*/ 
 
 function App() {
+  /* Gets the current date in M/D/Y string format using current 
+  Creates new Date object using the "date" reformatted string 
+  Convertes dateRef to a Date String, then an array 
+  Reformats dateArr as another string */ 
+  const current = new Date(); 
+  const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+  const dateRef = new Date(date); 
+  const dateArr = dateRef.toDateString().split(' '); 
+  const dateFormat = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3]; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="MacroTrack">
+        <h1> <code> MacroTrack </code> </h1> 
       </header>
+      <center> <font size = "+3"> {dateFormat} </font> </center> 
     </div>
   );
 }
